f505ffebef93be93856ee829ee00c2ce3031aa9e
melifes <zhanglin-pc@live.com>
2016-04-07 20:15:06 +0800

    fix

--- README.md
+++ README.md
@@ -1,4 +1,4 @@
-#NeteaseCloudMusicApi
+# NeteaseCloudMusicApi
 
 目前我只搜集到以下可用API：
 
@@ -7,4 +7,3 @@
 * 歌词API
 
 API返回的是json格式，前端可用js输出。具体使用请看index.php;
-
--- index.php
+++ index.php
@@ -9,20 +9,21 @@ $song_id = 407044249;
 $album_id = 18896;
 
 //返回搜索结果
-//传入 搜索内容，返回的条数，类型(默认1即可)，偏移量
-$result = $api->search($keyword, 1, 1, 0);
+//传入参数（搜索内容，返回的条数，偏移量）
+$result = $api->search($keyword, 1, 0);
 
 //返回歌曲详情
 $detail = $api->detail($song_id);
 
-//歌曲专辑
+//歌曲专辑（不可用）
 $albums = $api->albums($album_id, 30);
 
 //歌曲歌词
-$lyric = $api->lyric($song_id, 30);
+$lyric = $api->lyric($song_id);
 
 
 print_r($result);
 // print_r($detail);
 // print_r($albums);
 // print_r($lyric);
+
